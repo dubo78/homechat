4572e47c81876371f564d3c1b7ffdedcb47abb2b
dubo78 <napper78@gmail.com>
2026-03-15 19:28:41 +0900

    debuging

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 300,
-    height: 400,
+    width: 800, // 에러 창을 같이 보려면 크기를 넓히는 게 좋아
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
   win.loadFile('index.html');
+  win.webContents.openDevTools(); // 이 줄을 추가!
 }
 
 app.whenReady().then(createWindow);
